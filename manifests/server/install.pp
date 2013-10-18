2eab007c5f1530ff52e3e9a91880ad2aade63be6
Ashley Penney <ashley.penney@puppetlabs.com>
2013-10-18 13:59:50 -0400

    Make classes private and setup dependencies in `ssh`.

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,4 +1,8 @@
 # See README.md for further information on usage.
 class ssh::server::install {
 
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
+  }
+
 }
