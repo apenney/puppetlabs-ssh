19a55dc8c09dc329e326e555ef1af0497aea0d7c
Ashley Penney <ashley.penney@puppetlabs.com>
2013-10-22 22:27:05 -0400

    Add ssh_tunnel type and provider.
    This provider adds ssh_tunnel, a type/provider pair that allows the
    creation and management of ssh tunnels, both -L and -D varieties.

---
+++ lib/puppet/provider/ssh_tunnel/ssh.rb
@@ -0,0 +1,119 @@
+Puppet::Type.type(:ssh_tunnel).provide(:ssh) do
+  desc 'Create ssh tunnels'
+
+  commands :ssh => 'ssh'
+
+  def self.instances
+    instances = []
+    tunnel_connections = ssh_processes("#{ssh_opts} -L")
+    socks_connections  = ssh_processes("#{ssh_opts} -D")
+
+    tunnel_connections.each do |name, pid|
+      rest, remote_host = name.split(' ')
+      local_port, forward_server, forward_port = rest.split(':')
+      instances << new(
+        :ensure         => :present,
+        :name           => "#{rest}-#{remote_host}",
+        :local_port     => local_port,
+        :forward_server => forward_server,
+        :forward_port   => forward_port,
+        :remote_host    => remote_host,
+        :socks          => :false,
+        :pid            => pid
+      )
+    end
+
+    socks_connections.each do |name, pid|
+      local_port, remote_host = name.split(' ')
+
+      instances << new(
+        :ensure      => :present,
+        :name        => "#{local_port}-#{remote_host}",
+        :local_port  => local_port,
+        :remote_host => remote_host,
+        :socks       => :true,
+        :pid         => pid
+      )
+    end
+
+    return instances
+  end
+
+  def self.prefetch(resources)
+    # Obtain a hash of all the instances we can discover
+    tunnels = instances
+    resources.keys.each do |name|
+      # If we find the resource in the catalog in the discovered list
+      # then set the provider for the resource to this one.
+      if provider = tunnels.find { |tunnel| tunnel.name == name }
+        resources[name].provider = provider
+      end
+    end
+  end
+
+  def create
+    if @property_hash[:socks] == true
+      # Create a SOCKS proxy
+      ssh(
+        ssh_opts,
+        '-D',
+        @property_hash[:local_port],
+        @property_hash[:remote_server]
+      )
+      @property_hash[:ensure]        = :present
+      @property_hash[:local_port]    = @resource[:local_port]
+      @property_hash[:remote_server] = @resource[:remote_server]
+    else
+      # Create an SSH tunnel
+      ssh(
+        ssh_opts,
+        '-L',
+        "#{@property_hash[:local_port]}:#{@property_hash[:forward_server]}:#{@property_hash[:forward_port]}",
+        @property_hash[:remote_server]
+      )
+      @property_hash[:ensure]         = :present
+      @property_hash[:local_port]     = @resource[:local_port]
+      @property_hash[:forward_server] = @resource[:forward_server]
+      @property_hash[:forward_port]   = @resource[:forward_port]
+      @property_hash[:remote_server]  = @resource[:remote_server]
+    end
+
+    exists? ? (return true) : (return false)
+  end
+  
+  def destroy
+    Process.kill('SIGTERM', @property_hash[:pid].to_i)
+    @property_hash.clear
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present || false
+  end
+
+  mk_resource_methods
+
+  private
+
+  # -f tells ssh to go into the background just before command execution
+  # -N tells ssh not to execute remote commands
+  def self.ssh_opts
+    '-f -N'
+  end
+
+  # Find and return the ssh tunnel/proxy names and their associated pid.
+  def self.ssh_processes(pattern)
+    ssh_processes = {}
+    ps = Facter["ps"].value
+    IO.popen(ps) do |table|
+      table.each_line do |line|
+        # Attempts to match 'user (pid).*ssh {sshopts} (port:host:port host)'
+        if match = line.match(/^\w+\s+(\d+)\s.*ssh\s#{pattern}\s(\d+:?\w*:?\d*\s\w.+)/)
+          pid, name = match.captures
+          ssh_processes[name] = pid
+        end
+      end
+    end
+    return ssh_processes
+  end
+
+end
---
+++ lib/puppet/type/ssh_tunnel.rb
@@ -0,0 +1,54 @@
+Puppet::Type.newtype(:ssh_tunnel) do
+  @doc = 'Manage SSH tunnels'
+
+  validate do
+    # If we're not in SOCKS mode, we need a server and port to forward to.
+    if self[:socks] == :false
+      fail('forward_server is not set') unless self[:forward_server].nil?
+      fail('forward_port is not set') unless self[:forward_port].nil?
+    end
+  end
+
+  # This automatically creates the ensure parameter.
+  ensurable
+
+  newparam(:name, :namevar => true) do
+    desc "The SSH tunnel name, must be unique."
+  end
+
+  newproperty(:socks) do
+    desc 'Should this be a SOCKS proxy'
+    newvalues(:true, :false)
+    defaultto :false
+  end
+
+  newproperty(:local_port) do
+    desc 'The local port to forward traffic from'
+    validate do |port|
+      fail("Port is not in the range 1-65535") unless port.to_i >= 1 and port.to_i <= 65535
+    end
+  end
+
+  newproperty(:forward_port) do
+    desc 'Port to forward traffic to'
+    validate do |port|
+      fail("Port is not in the range 1-65535") unless port.to_i >= 1 and port.to_i <= 65535
+    end
+  end
+
+  newproperty(:remote_server) do
+    desc 'Remote server to connect to'
+    newvalue(/\w+/)
+  end
+
+  newproperty(:forward_server) do
+    desc 'Server to forward traffic to'
+    newvalue(/\w+/)
+  end
+
+  newproperty(:pid) do
+    desc 'pid of process.'
+    newvalue(/\d+/)
+  end
+ 
+end
